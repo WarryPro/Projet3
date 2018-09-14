81e47b333f7c74175f15794ef8c3ce47cb3f45e8
WarryPro <dannyfr.03@gmail.com>
2018-09-14 09:37:54 +0200

    Créé fichier forgotpassword.php pour la récuperation d'un mdp

---
+++ Views/frontend/forgotpassword.php
@@ -0,0 +1,31 @@
+<?php $title = 'connection';?>
+
+<?php ob_start(); ?>
+
+    <div class="grid-container">
+
+        <div class="form-container">
+
+            <form action="index.php?action=forgotpassword" method="POST" class="form">
+                <h2 class="form-group">Veuillez saisir votre email</h2>
+
+                <div class="grid-x">
+                    <div class="form-group">
+
+                        <div class="small-12 medium-4">
+                            <input type="email" name="email" id="email" class="email form-control" required>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-valider" value="valider"></input>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+<?php $content = ob_get_clean();?>
+
+<?php require ('template.php');?>
