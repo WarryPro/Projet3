1793ab83f6789152cd8608a2ca2bd6e60c150152
WarryPro <dannyfr.03@gmail.com>
2018-09-22 01:37:17 +0200

    ajouté conteneur des reseaux socieux dans le header

--- Views/frontend/header.php
+++ Views/frontend/header.php
@@ -1,8 +1,14 @@
-<div class="grid-container header cell grid-x">
-        <div class="cell small-4 container-title">
+<div class="grid-container header grid-x">
+        <div class="small-12 grid-x container-title">
             <h1 class="main-header__title">Jean Forteroche Blog!</h1>
+            <div class="reseaux-socieux">
+                <a href="#" class="reseau-link facebook fab fa-facebook-f"></a>
+                <a href="#" class="reseau-link twitter fab fa-twitter"></a>
+                <a href="#" class="reseau-link linkedin fab fa-linkedin-in"></a>
+                <a href="#" class="reseau-link instagram fab fa-instagram"></a>
+            </div>
         </div>
-        <div class="container-main-menu cell auto">
+        <div class="container-main-menu grid-x small-12">
             <nav class="main-nav">
                 <ul class="main-menu menu align-right">
                     <li class="main-menu__item">
