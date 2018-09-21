e89042e74fed6da55f7b76068178bfc4e7975200
WarryPro <dannyfr.03@gmail.com>
2018-09-22 01:33:26 +0200

    Ajouté fontawesome, enlevé google fonts Moserrat, ajouté la balise main avec les classes grid-x et grid-container

--- Views/frontend/template.php
+++ Views/frontend/template.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $title ?></title>
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.5.0-rc.2/dist/css/foundation.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link href="public/css/styles.css" rel="stylesheet" />
     </head>
         
@@ -16,7 +16,9 @@
             ?>
         </header>
 
-        <?= $content; ?>
+        <main class="grid-x grid-container">
+            <?= $content; ?>
+        </main>
 
         <footer class="grid-x">
         <!-- footer content -->
