09ef0dd543eff6aae9b2116ad7014e71d6983748
WarryPro <dannyfr.03@gmail.com>
2018-09-23 00:16:36 +0200

    créé module du menu des reseaux s.

---
+++ Views/frontend/partials/menuReseauxSocieux.php
@@ -0,0 +1,6 @@
+<div class="reseaux-socieux">
+    <a href="#" class="reseau-link facebook fab fa-facebook-f"></a>
+    <a href="#" class="reseau-link twitter fab fa-twitter"></a>
+    <a href="#" class="reseau-link linkedin fab fa-linkedin-in"></a>
+    <a href="#" class="reseau-link instagram fab fa-instagram"></a>
+</div>
