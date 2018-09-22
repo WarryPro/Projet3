f59822f3877ee545466b5433ed9ff53198b92dfa
WarryPro <dannyfr.03@gmail.com>
2018-09-23 00:24:01 +0200

    enlevé le code pour le menu des reseaux s. et transformé dans un module ('partials/menuReseauxSociaux.php'

--- Views/frontend/header.php
+++ Views/frontend/header.php
@@ -1,13 +1,11 @@
 <div class="grid-container header grid-x">
-        <div class="small-12 grid-x container-title">
-            <h1 class="main-header__title">Jean Forteroche Blog!</h1>
-            <div class="reseaux-socieux">
-                <a href="#" class="reseau-link facebook fab fa-facebook-f"></a>
-                <a href="#" class="reseau-link twitter fab fa-twitter"></a>
-                <a href="#" class="reseau-link linkedin fab fa-linkedin-in"></a>
-                <a href="#" class="reseau-link instagram fab fa-instagram"></a>
-            </div>
-        </div>
+    <div class="small-12 grid-x container-title">
+        <h1 class="main-header__title">Jean Forteroche Blog!</h1>
+
+        <?php
+            require('partials/menuReseauxSociaux.php');
+        ?>
+    </div>
         <div class="container-main-menu grid-x small-12">
             <nav class="main-nav">
                 <ul class="main-menu menu align-right">
