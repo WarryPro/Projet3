dafab1b3687b86db703beb862854f89b6114cf04
WarryPro <dannyfr.03@gmail.com>
2018-09-23 00:25:21 +0200

    sup code innecessaire et ajouté le partiel du menu des réseaux sociaux

--- Views/frontend/footer.php
+++ Views/frontend/footer.php
@@ -1,44 +1,8 @@
-<section class="container-footer grid-container cell grid-x">
-    <div class="container-footer__item text-center small-12 medium-4 large-4">
-        <h3 class="footer-title">LA SOCIÉTÉ</h3>
-        <ul class="footer-menu">
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">A propos de nous</a>
-            </li>
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">Contact</a>
-            </li>
-        </ul>
-    </div>
-    <div class="container-footer__item text-center small-12 medium-4 large-4">
-        <h3 class="footer-title">LA SOLUTION MYPROPERTY</h3>
-        <ul class="footer-menu">
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">Créez votre espace</a>
-            </li>
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">Tarifs</a>
-            </li>
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">FAQ</a>
-            </li>
-        </ul>
-    </div>
-    <div class="container-footer__item text-center small-12 medium-4 large-4">
-        <h3 class="footer-title">LEGAL</h3>
-        <ul class="footer-menu">
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">Conditions générales</a>
-            </li>
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">Mentions légales</a>
-            </li>
-            <li class="footer-menu__item">
-                <a class="footer-menu__link" href="#">Confidentialité</a>
-            </li>
-        </ul>
-    </div>
-</section>
-<div class="container-copyright grid-x grid-y cell small-12">
-    <p class="copyright text-center"> Myproperty | 2018 © Tous droits réservés. </p>
+<section class="container-footer grid-container grid-x">
+
+    <?php  require('partials/menuReseauxSociaux.php'); ?>
+
+<div class="container-copyright grid-x grid-y small-12">
+    <p class="copyright text-center">JEAN FORTEROCHE | 2018 © Tous droits réservés. </p>
 </div>
+</section>
