2651771cabb0b205c2c518b006d3dff2f2300afa
WarryPro <dannyfr.03@gmail.com>
2018-10-01 01:08:06 +0200

    menu lateral pour la page post

---
+++ Views/frontend/postAside.php
@@ -0,0 +1,45 @@
+
+<div class="post-aside small-12 large-4">
+
+    <div class="post-header">
+
+        <div class="btn-menu">
+            Menu
+        </div>
+
+        <div class="post-header-blog">
+            <a class="main-header__title">JEAN FORTEROCHE BLOG!</a>
+        </div>
+
+        <div class="container-post-menu grid-x small-12">
+            <nav class="main-nav">
+                <ul class="post-menu menu vertical">
+                    <li class="main-menu__item">
+                        <a href="./index.php" class="main-menu__link">Accueil</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a href="./index.php?action=billets" class="main-menu__link">Billets</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a href="./index.php?action=contact" class="main-menu__link">Contact</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a href="./index.php?action=connexion" class="main-menu__link">Se connecter</a>
+                    </li>
+                    <li class="main-menu__item">
+                        <a href="./index.php?action=inscription" class="main-menu__link">S'inscrire</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </div>
+
+    <div class="container-titre-livre">
+        <p class="auteur">Jean Forteroche</p>
+        <h1 class="titre-livre">BILLET SIMPLE POUR L'ALASKA</h1>
+    </div>
+
+</div>
+
+
