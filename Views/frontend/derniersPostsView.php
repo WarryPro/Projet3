77e4155f7c1de934cfa11ba0663c9d4663e6b871
WarryPro <dannyfr.03@gmail.com>
2018-10-01 01:11:44 +0200

    nombre statiques de comments enlevés pour l'instant (à recupérer après dynamiquement)

--- Views/frontend/derniersPostsView.php
+++ Views/frontend/derniersPostsView.php
@@ -27,7 +27,7 @@
                             <?= substr(htmlspecialchars($data['content']), 0,200) .'...' ?>
                         </p>
                         <div class="card__btns grid-x">
-                            <a class="card__comment-link far fa-comment" href="index.php?action=post&amp;id=<?= $data['id'] ?>"><span> <i class="nb-comments"> 14</i> Commentaires</span></a>
+                            <a class="card__comment-link far fa-comment" href="index.php?action=post&amp;id=<?= $data['id'] ?>"><span> <i class="nb-comments"> <!-- mettre le nombre de commentaires dynamiquement pour ce billet --></i> Commentaires</span></a>
                             <a class="card__share-link fas fa-share-alt" href="#"> <span> Share</span></a>
                         </div>
                     </div>
