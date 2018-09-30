06e03312c7106d334893c84414fff3d126b5e412
WarryPro <dannyfr.03@gmail.com>
2018-10-01 01:09:46 +0200

    en procesus de modif l'estructure de la page post

--- Views/frontend/postView.php
+++ Views/frontend/postView.php
@@ -3,60 +3,67 @@
 <?php ob_start(); ?>
 
 
-<main class="grid-container main-container grid-x">
-    <p><a class="btn-primary" href="index.php">Retour à la liste des billets</a></p>
-    <div class="cell">
-            <div class="news-container grid-y">
-                <div class="news-title cell">
-                    <h1>
+<section class="post-main-container grid-x">
+
+    <?php require('postAside.php') ?>
+
+    <div class="post-container small-12 large-8">
+
+        <?php require('partials/bannerPost.php') ?>
+
+        <div class="small-12 medium-6 large-8">
+            <div class="post grid-y">
+                <div class="post-title ">
+                    <h2>
                         <?= htmlspecialchars($post['title']) ?>
-                    </h1>
-                    <span class="news-date"><em><?= $post['created_date_fr'] ?></em></span>
+                    </h2>
+                    <span class="post-date"><em><?= $post['created_date_fr'] ?></em></span>
                 </div>
-                <div class="news-content cell">
+                <div class="post-content ">
                     <p>
                         <?= nl2br(htmlspecialchars($post['content'])) ?>
                     </p>
                 </div>
+            </div>
+        </div>
+
+        <div class=" grid-x post-main-container form-comments">
+            <div class="grid-container post-main-container form-comments grid-x ">
+                <h2 class=" subtitle">Commentaires</h2>
+            <div class=" grid-container grid-x container-form">
+                <form class=" small-6" action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+                    <div class="input-form">
+                        <label for="user">Utilisateur</label><br />
+                        <input type="text" id="user" name="user" />
+                    </div>
+                    <div class="input-form">
+                        <label for="comment">Commentaire</label><br />
+                        <textarea id="comment" name="comment"></textarea>
+                    </div>
+                    <div class="btn-container">
+                        <input class="btn-primary btn-form"type="submit" />
+                    </div>
+                </form>
+            </div>
+            <div class="container-comments grid-container ">
+                <?php
+                while ($comment = $comments->fetch()) {
+                ?>
+
+                    <p><strong><?= htmlspecialchars($comment['user']) ?></strong>
+
+                    <span class="comment-date">le <?= $comment['comment_date_fr'] ?></span> </p>
+
+                    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+                <?php
+                }
+                ?>
+                <?php $content = ob_get_clean(); ?>
+
+                <?php require('template.php'); ?>
+
+            </div>
+        </div>
         </div>
     </div>
-
-    <div class="cell grid-x main-container form-comments">
-        <div class="grid-container main-container form-comments grid-x cell">
-            <h2 class="cell subtitle">Commentaires</h2>
-        <div class="cell grid-container grid-x container-form">
-            <form class="cell small-6" action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-                <div class="input-form">
-                    <label for="user">Utilisateur</label><br />
-                    <input type="text" id="user" name="user" />
-                </div>
-                <div class="input-form">
-                    <label for="comment">Commentaire</label><br />
-                    <textarea id="comment" name="comment"></textarea>
-                </div>
-                <div class="btn-container">
-                    <input class="btn-primary btn-form"type="submit" />
-                </div>
-            </form>
-        </div>
-        <div class="container-comments grid-container cell">
-            <?php
-            while ($comment = $comments->fetch()) {
-            ?>
-
-                <p><strong><?= htmlspecialchars($comment['user']) ?></strong> 
-                
-                <span class="comment-date">le <?= $comment['comment_date_fr'] ?></span> </p>
-
-                <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-            <?php
-            }
-            ?>
-            <?php $content = ob_get_clean(); ?>
-            
-            <?php require('template.php'); ?>
-        
-        </div>
-    </div>
-    </div>
-</main>
+</section>
