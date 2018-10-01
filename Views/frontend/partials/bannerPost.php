7fe39df314d218cdd3949ca5ddb1fb9598d37817
WarryPro <dannyfr.03@gmail.com>
2018-10-02 01:30:14 +0200

    changée la photo du banner

--- Views/frontend/partials/bannerPost.php
+++ Views/frontend/partials/bannerPost.php
@@ -1,3 +1,3 @@
 <div class="banner-post banner">
-    <img src="https://images.pexels.com/photos/35637/alaska-glacier-ice-mountains.jpg?auto=compress" alt="Banner" class="banner__img">
+    <img src="https://images.pexels.com/photos/291732/pexels-photo-291732.jpeg?auto=compress" alt="Banner" class="banner__img">
 </div>
