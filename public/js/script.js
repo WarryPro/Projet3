3c450441bb8b8e0ca90172348d02501b561ef406
WarryPro <dannyfr.03@gmail.com>
2018-12-15 01:04:52 +0100

    verification si l'elmt de class .post-main-container existe

--- public/js/script.js
+++ public/js/script.js
@@ -15,4 +15,12 @@ const textareaLabel = document.querySelector('.label-form')
     })
 
 
+// Page postView
+const bodyPagePost = document.querySelector("body")
+const postMainContainer = document.querySelector("main > .post-main-container")
+
+// verifier si l'elmt existe
+if(typeof postMainContainer === "object") {bodyPagePost.className = "body-bg-post"}
+
+
 
