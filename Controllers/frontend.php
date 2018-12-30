83670ee377041018e789c899a3d00648e5abd28c
WarryPro <dannyfr.03@gmail.com>
2018-12-30 01:44:46 +0100

    Création fonction pour gérér les vues si admin ou user et créé la fonction pour ajouter un post

--- Controllers/frontend.php
+++ Controllers/frontend.php
@@ -6,14 +6,34 @@ use \Models\CommentManager;
 require_once('Models/PostManager.php');
 require_once('Models/CommentManager.php');
 
+
 function listPosts() {
-    
+
     $postManager = new PostManager(); // Création de l'objet postManager
 
     $posts = $postManager->getPosts(); // Appel de la methode getPosts() de cet objet
 
     require('Views/frontend/listePostsView.php');
 }
+
+
+
+function adminListPosts() {
+
+    $postManager = New PostManager(); // Création de l'objet postManager
+
+    if($_SESSION['user_role'] === 'Admin') {
+
+        $posts = $postManager->getPosts(); // Appel de la methode getPosts() de cet objet
+
+        require('Views/backend/addPostsView.php');
+    }
+    else {
+        header('location: ./index.php');
+    }
+
+}
+
 
 function derniersPosts() {
 
@@ -36,6 +56,26 @@ function post() {
     $comments = $commentManager->getComments($_GET['id']);
 
     require('views/frontend/postView.php');
+}
+
+
+function addPosts($id = '0', $title, $content, $image = '') {
+
+    $postManager = new PostManager(); // Création de l'objet postManager
+
+    $affectedLines = $postManager->addPost($id, $title, $content, $image);
+
+    //On teste donc s'il y a eu une erreur et on arrête tout si jamais il y a eu un souci.
+    if ($affectedLines === false) {
+
+        die('Impossible d\'ajouter le post !');
+    }
+
+    //Les données ont été insérées, on redirige donc le visiteur vers la page du billet pour qu'il puisse voir son commentaire
+    else {
+
+        header('Location: index.php?action=admin');
+    }
 }
 
 
