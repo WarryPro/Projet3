ba08a0a3108ca4a79cf6bab93fcc4fe494c9a9ae
WarryPro <dannyfr.03@gmail.com>
2019-01-03 02:01:41 +0100

    View admin crud btns

---
+++ Views/frontend/components/crudBtnView.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danny
+ * Date: 02/01/2019
+ * Time: 22:58
+ */
+
+
+    print_r(
+        '<div class="admin-crud">
+                    <div class="admin-crud_container">
+                        <a href="#" class="admin-crud__btn admin-crud__btn--editer">Editer</a>
+                        <a href="#" class="admin-crud__btn admin-crud__btn--supprimer">Supprimer</a>
+                    </div>
+              </div>'
+    );
