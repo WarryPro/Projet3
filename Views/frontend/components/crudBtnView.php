781907a3ff1eab66c9f6615f0fbd2114b8f228a5
WarryPro <dannyfr.03@gmail.com>
2019-01-04 01:38:43 +0100

    ajouté les id's pour editer ou supp un post

--- Views/frontend/components/crudBtnView.php
+++ Views/frontend/components/crudBtnView.php
@@ -1,17 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: danny
- * Date: 02/01/2019
- * Time: 22:58
- */
 
-
-    print_r(
-        '<div class="admin-crud">
-                    <div class="admin-crud_container">
-                        <a href="#" class="admin-crud__btn admin-crud__btn--editer">Editer</a>
-                        <a href="#" class="admin-crud__btn admin-crud__btn--supprimer">Supprimer</a>
-                    </div>
-              </div>'
-    );
+<div class='admin-crud'>
+    <div class='admin-crud_container'>
+        <a href='index.php?action=editer&amp;id=<?= $data['id'] ?>' class='admin-crud__btn admin-crud__btn--editer'>Editer</a>
+        <a href='index.php?action=supprimer&amp;id=<?= $data['id'] ?>' class='admin-crud__btn admin-crud__btn--supprimer'>Supprimer</a>
+    </div>
+</div>
