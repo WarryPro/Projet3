327a58d84ded081f20d999e254737baccf544840
WarryPro <dannyfr.03@gmail.com>
2019-01-10 01:13:01 +0100

    supp le containeur de l'alerte quand cette derniere est disparue

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,9 +1,10 @@
 const alert = document.getElementById('alert');
-
+const alertContainer = document.getElementById('alert-container');
 if(alert !== null) {
     alert.classList.add('show')
     setTimeout(() => {
         alert.classList.remove('show')
         alert.parentNode.removeChild(alert)
+        alertContainer.parentNode.removeChild(alertContainer)
     }, 5000)
 }
