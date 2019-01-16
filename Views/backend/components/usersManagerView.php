cc3269c5203a1567efce02a338f0f9ee5feec685
WarryPro <dannyfr.03@gmail.com>
2019-01-17 00:56:14 +0100

    créé vue pour la gestion d'utilisateurs

---
+++ Views/backend/components/usersManagerView.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danny
+ * Date: 16/01/2019
+ * Time: 21:52
+ */
+
+?>
+
+<div class="small-12 grid-x">
+    <div class="user-manager-container grid-x small-12">
+        <div class="user grid-x medium-6">
+            <div class="user__info grid-x">
+                <div class="user__photo">
+                    <img src="./public/images/dany.jpg" alt="Photo Dany">
+                </div>
+                <div class="grid-y">
+                    <h3 class="user__name">Name Lastname</h3>
+                    <span class="user__rol">Role</span>
+                </div>
+            </div>
+
+            <div class="user__controls small-12">
+                <a href="#" class="btn admin-crud__btn admin-crud__btn--editer">Editer</a>
+                <a href="#" class="btn admin-crud__btn admin-crud__btn--supprimer">Supprimer</a>
+            </div>
+        </div>
+
+        <div class="user grid-x medium-6">
+            <div class="user__info grid-x">
+                <div class="user__photo">
+                    <img src="./public/images/dany.jpg" alt="Photo Dany">
+                </div>
+                <div class="grid-y">
+                    <h3 class="user__name">Name Lastname</h3>
+                    <span class="user__rol">Role</span>
+                </div>
+            </div>
+
+            <div class="user__controls small-12">
+                <a href="#" class="btn admin-crud__btn admin-crud__btn--editer">Editer</a>
+                <a href="#" class="btn admin-crud__btn admin-crud__btn--supprimer">Supprimer</a>
+            </div>
+        </div>
+    </div>
+</div>
