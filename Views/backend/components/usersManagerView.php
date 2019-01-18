6dc37f77113503c94899cabe91baffc4fe9ddd73
WarryPro <dannyfr.03@gmail.com>
2019-01-18 20:35:36 +0100

    modifiée struct user card

--- Views/backend/components/usersManagerView.php
+++ Views/backend/components/usersManagerView.php
@@ -13,22 +13,25 @@ $data = listUsers();
     <?php
     foreach ($data as $user) {
         print_r(
-'<div class="user grid-x medium-6">
-                <div class="user__info grid-x">
-                    <div class="user__photo">
-                        <img src="./public/images/'.$user['user'].'.jpg" alt="Photo Dany">
+'<div class="user grid-x medium-6 large-4">
+                <div class="user__info-container user-card grid-x small-12">
+                    <div class="user__info grid-x">
+                        <div class="user__photo">
+                            <img src="./public/images/'.$user['user'].'.jpg" alt="Photo Dany">
+                        </div>
+                        
+                        <div class="grid-y">
+                            <h3 class="user__name">'. $user['user'] .'</h3>
+                            <span class="user__rol">'. $user['user_role'] .'</span>
+                        </div>
                     </div>
                     
-                    <div class="grid-y">
-                    <h3 class="user__name">'. $user['user'] .'</h3>
-                    <span class="user__rol">'. $user['user_role'] .'</span>
+                    <div class="user__controls small-12">
+                        <a href="index.php?action=editeruser&id='. $user['id'].'" class="btn admin-crud__btn admin-crud__btn--editer"><i class="fas fa-user-edit"></i></a>
+                        <a href="index.php?action=supprimeruser&id='. $user['id'].'" class="btn admin-crud__btn admin-crud__btn--supprimer"><i class="fas fa-user-times"></i></a>
+                    </div>
                 </div>
-            </div>
-            <div class="user__controls small-12">
-                <a href="index.php?action=editeruser&id='. $user['id'].'" class="btn admin-crud__btn admin-crud__btn--editer">Editer</a>
-                <a href="index.php?action=supprimeruser&id='. $user['id'].'" class="btn admin-crud__btn admin-crud__btn--supprimer">Supprimer</a>
-            </div>
-        </div>');
+          </div>');
     }
     ?>
     </div>
