6f3824a6c27b727db4ee31c2903a01f3e40066ca
WarryPro <dannyfr.03@gmail.com>
2019-01-28 01:42:26 +0100

    ajoutés des attrs pour les récupérer avec JS

--- Views/backend/components/usersManagerView.php
+++ Views/backend/components/usersManagerView.php
@@ -17,18 +17,23 @@ $data = listUsers();
                 <div class="user__info-container user-card grid-x small-12">
                     <div class="user__info grid-x">
                         <div class="user__photo">
-                            <img src="./public/images/'.$user['user'].'.jpg" alt="Photo Dany">
+                            <img id="user-photo" src="./public/images/'.$user['user'].'.jpg" alt="Photo Dany">
                         </div>
                         
                         <div class="grid-y">
                             <h3 class="user__name">'. $user['user'] .'</h3>
+                            <span class="user__email">'. $user['email'] .'</span>
                             <span class="user__rol">'. $user['user_role'] .'</span>
                         </div>
                     </div>
                     
                     <div class="user__controls small-12">
-                        <a href="#modal-user-edit" class="btn btn__user--edit admin-crud__btn admin-crud__btn--editer"><i class="fas fa-user-edit"></i></a>
-                        <a href="index.php?action=supprimeruser&id='. $user['id'].'" class="btn btn__user--supprimer admin-crud__btn admin-crud__btn--supprimer"><i class="fas fa-user-times"></i></a>
+                        <a href="#modal-user-edit?userid='.$user['id'].'" class="btn btn__user--edit admin-crud__btn admin-crud__btn--editer" data-id="'.$user['id'].'">
+                            <i class="fas fa-user-edit"></i>
+                        </a>
+                        <a href="index.php?action=supprimeruser&id='. $user['id'].'" class="btn btn__user--supprimer admin-crud__btn admin-crud__btn--supprimer">
+                            <i class="fas fa-user-times"></i>
+                        </a>
                     </div>
                 </div>
           </div>');
