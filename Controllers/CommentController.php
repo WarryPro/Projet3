c42b572a6665a73a4b17098a28cdb258ff6d0352
WarryPro <dannyfr.03@gmail.com>
2019-02-16 23:56:38 +0100

    Controller methode reportComment

--- Controllers/CommentController.php
+++ Controllers/CommentController.php
@@ -7,7 +7,6 @@
  */
 
 namespace Controllers;
-//use \Models\Manager;
 use \Models\CommentManager;
 use \entity\ReportComment;
 
@@ -17,14 +16,18 @@ class CommentController {
     public function reportComment(ReportComment $report) {
 
         $commentManager = New CommentManager();
-        $reportComment = $commentManager -> reportComment($report->getId());
+//        $uri = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $reportComment = $commentManager -> reportComment($report);
 
         if($reportComment === FALSE) {
 
             echo "Une erreur est survenu";
         }
         else {
-            header('location: ./index.php?action=post&id='.$report->getId());
+            $sessionControler = New SessionController();
+            $sessionControler->setFlash('Le commentaire a été signalé', 'success');
+            header('location: ../index.php?action=post&id='. $_SESSION['uri']);
+            unset($_SESSION['uri']); // Supp la session pour cette var
         }
     }
 }
