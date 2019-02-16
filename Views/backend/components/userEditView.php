eeaf8d449849496ce05ed07eb2ee85acc986eac3
WarryPro <dannyfr.03@gmail.com>
2019-02-17 00:04:20 +0100

    View Modal éditer user

--- Views/backend/components/userEditView.php
+++ Views/backend/components/userEditView.php
@@ -3,6 +3,7 @@
 
 <div id="modal" class="container-login container-user-edit hide">
     <div class="main-container-form card">
+        <div id="reponse"></div>
         <div id="btn-close" class="btn-close"></div>
         <div class="container-photo-profile">
             <div class="photo-profile">
@@ -10,7 +11,8 @@
             <p class="text-connexion">Editer utilisateur</p>
         </div>
         <div class="container-form grid-x">
-            <form action="index.php?action=updateuser" method="POST" class="small-10">
+<!--            action="index.php?action=updateuser"-->
+            <form action="index.php?action=updateuser" id="form-user--edit" method="POST" class="small-10">
                 <input type="hidden" name="user-id" id="user-id" value="">
                 <div class="form-group">
                     <span class="fas fa-user icon-user"></span>
