fb8c1a7cebae684f733d060e884532f30b4a7bbc
WarryPro <dannyfr.03@gmail.com>
2019-02-17 20:54:34 +0100

    validation dans la méthode ReportComment si l'utilisateur a déjà signalé  le comment, création de la méthode userHasReported pour valider si l'utilisateur a déjà signalé un commentaire

--- Controllers/CommentController.php
+++ Controllers/CommentController.php
@@ -12,22 +12,60 @@ use \entity\ReportComment;
 
 class CommentController {
 
-    //TODO: signaler un commentaire
+/**
+ * méthode controlleur pour signaler un commentaire
+ * @param $report prend les données passe par la class ReportComment et
+ * les envoie à la méthode reportComment de la class CommentManager
+ */
     public function reportComment(ReportComment $report) {
 
-        $commentManager = New CommentManager();
+        $sessionControler = New SessionController();
+
+        $userAccuser = $this-> userHasReported($report->getCommentId());
+
+//        si l'utilisateur n'a pas encore signalé le commentaire
+        if(!$userAccuser) {
+
+            $commentManager = New CommentManager();
 //        $uri = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        $reportComment = $commentManager -> reportComment($report);
+            $reportComment = $commentManager -> reportComment($report);
 
-        if($reportComment === FALSE) {
+            if($reportComment === FALSE) {
 
-            echo "Une erreur est survenu";
+                echo "Une erreur est survenu";
+            }
+            else {
+                $sessionControler->setFlash('Le commentaire a été signalé', 'success');
+                header('location: ../index.php?action=post&id='. $_SESSION['uri']);
+                unset($_SESSION['uri']); // Supp la session pour cette var
+            }
         }
+
+//        si il a déjà signalé avant alors en envoie une alerte
         else {
-            $sessionControler = New SessionController();
-            $sessionControler->setFlash('Le commentaire a été signalé', 'success');
+
+            $sessionControler->setFlash('Vous avez déjà signalé ce commentaire...', 'error');
             header('location: ../index.php?action=post&id='. $_SESSION['uri']);
             unset($_SESSION['uri']); // Supp la session pour cette var
+        }
+
+    }
+
+    /*
+     * Return un bool si l'utilisateur returné par la méthode userHasReported de la Class CommentManager
+     * est = ou != à l'utilisateur en SESSION
+     * */
+    public function userHasReported ($commentId) {
+
+        $commentManager = New CommentManager();
+        $userAcusser = $_SESSION['user'];
+
+        $userDB = $commentManager -> userHasReported($commentId);
+        if($userDB === $userAcusser) {
+            return true;
+        }
+        else {
+           return false;
         }
     }
 }
