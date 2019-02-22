8c7125d6d4914f492dce9e1780b873a7f78d2edf
WarryPro <dannyfr.03@gmail.com>
2019-02-22 01:37:15 +0100

    affichage tableau de commentaire signalés avec ses boutons d'actions

--- Views/backend/components/commentsManagerView.php
+++ Views/backend/components/commentsManagerView.php
@@ -8,13 +8,15 @@
 
 ?>
 
-<div class="small-12 grid-x">
-    <table class="hover table-scroll">
+<div class="small-12 grid-x pd-top-btm">
+    <table class="stack table-scroll">
         <thead>
             <tr>
+                <th>Épisode</th>
+                <th>ID épisode</th>
                 <th>Commentaire</th>
                 <th>Utilisateur signaleur</th>
-                <th>ID épisode</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -23,9 +25,18 @@
             foreach($reportedComments as $comment) {
                 print_r('
                         <tr>
+                            <td><a class="btn--alt valider--alt" href="./index.php?action=post&id='. $comment["episode_id"] .'">Voir l\'épisode</a></td>
+                            <td>' . $comment["episode_id"] . '</td>
                             <td>' . $comment["reported_comment"] . '</td>
                             <td>' . $comment["user_accuser"] . '</td>
-                            <td>' . $comment["episode_id"] . '</td>
+                            <td class="action-bouttons">
+                                <a class="btn--alt error--alt" href="index.php?action=supreportedcom&id='. $comment['comment_id'].'">
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                                <a class="btn--alt valider" href="index.php?action=hidereportedcom&id='. $comment['comment_id'].'">
+                                    <i class="fas fa-eye-slash"></i>
+                                </a>
+                            </td>
                         </tr>
                 ');
         }
