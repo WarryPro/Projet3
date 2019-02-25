7be2aee35cac477d466c1cf4d61e2cdca2925347
WarryPro <dannyfr.03@gmail.com>
2019-02-26 00:59:53 +0100

    compossant pagination View

---
+++ Views/frontend/components/pagination.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danny
+ * Date: 26/02/2019
+ * Time: 00:26
+ */
+
+?>
+
+<nav class="nav-pagination" aria-label="Pagination">
+    <ul class="pagination text-center">
+        <li class="pagination__item pagination-previous disabled">Start</li>
+        <li class="pagination__item current">
+            <span class="show-for-sr">You're on page</span> 1</li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">2</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">3</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">4</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link">5</a>
+        </li>
+        <li class="pagination__item">
+            <a href="#" class="pagination__link" aria-label="Next page">Next</a>
+        </li>
+    </ul>
+</nav>
