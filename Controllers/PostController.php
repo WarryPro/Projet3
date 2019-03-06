c3a27e5e0e35f882566d92f1113349a531818d9e
WarryPro <dannyfr.03@gmail.com>
2019-03-06 22:13:53 +0100

    utilisation de la méthode getSessionRole et flash

--- Controllers/PostController.php
+++ Controllers/PostController.php
@@ -18,18 +18,19 @@ class PostController {
     static function addPosts($id = '0', $title, $content, $image = '') {
         $bdd = New Manager();
         $bdd -> dbConnect();
+        $sessionController = New SessionController();
 
         $postManager = new PostManager(); // Création de l'objet postManager
 
         $affectedLines = $postManager->addPost($id, $title, $content, $image);
 
-        $sessionRole = (isset($_SESSION['user_role'])) ? $_SESSION['user_role'] : NULL;
+        $sessionRole = $sessionController -> getSessionRole();
         //On teste donc s'il y a eu une erreur et on arrête tout si jamais il y a eu un souci.
         if ($affectedLines === false && $sessionRole !== 'Admin') {
 
             echo "Impossible d'ajouter le post !";
         }
-
+        $sessionController -> setFlash('L\'épisode a été ajouté avec succès!', 'success');
         //Les données ont été insérées, on redirige donc le visiteur vers la page admin
         header('Location: index.php?action=admin');
     }
