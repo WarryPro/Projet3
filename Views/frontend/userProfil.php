494273f030aa22e1ba8bdc1f69bdbaae90498f0b
dany <dannyfr.03@gmail.com>
2019-03-23 18:58:26 +0100

    Ajouté flash message pour la mise à jour du profil

--- Views/frontend/userProfil.php
+++ Views/frontend/userProfil.php
@@ -16,12 +16,17 @@ $sessionController = New \Controllers\SessionController();
 </div>
 
 <div class="container-profil profil-info small-12 medium-4 large-3">
-    <form id="form-editer-profil" action="editprofil.php" class="form form-edit-profil grid-x align-center">
+    <div class="alert-container">
+        <?php echo $sessionController -> getFlash();
+        unset($_SESSION['flash']);
+        ?>
+    </div>
+    <form id="form-editer-profil" action="index.php?action=editprofil" method="POST" enctype="multipart/form-data" class="form form-edit-profil grid-x align-center">
         <button title="Éditer profil" id="btn-editer-profil" class="button warning editer"><i class="fas fa-pencil-alt"></i></button>
         <button title="annuler" id="btn-annuler" class="button error hide"><i class="fas fa-times"></i></button>
         <div class="profil-image">
             <img src="./<?= $user["user_image"]; ?>" alt="Image profil" class="image">
-            <input type="file" name="image-profil" id="input-image-profil" class="button hide">
+            <input type="file" name="image-profil" multiple id="input-image-profil" class="button hide">
             <label id="editer-image-profil" for="input-image-profil" class="button hide"><i class="fas fa-pencil-alt"></i></label>
         </div>
         <div class="profil profil__name text-center pd-t small-12">
@@ -34,7 +39,7 @@ $sessionController = New \Controllers\SessionController();
             </div>
             <div id="changermdp" class="form-group hide">
                 <p class="text-center">Changer le mot de passe</p>
-                <input type="password" id="current-pass" name="new-password" placeholder="mot de passe actuel">
+                <input type="password" id="current-pass" name="current-password" placeholder="mot de passe actuel">
                 <input type="password" id="new-pass" name="new-password" placeholder="Nouvel mot de passe">
             </div>
 
