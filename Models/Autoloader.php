e10dc344873c972c97e1528dec49397b4654e845
dany <dannyfr.03@gmail.com>
2019-03-24 23:03:43 +0100

    clean code autoloaders

--- Models/Autoloader.php
+++ Models/Autoloader.php
@@ -26,32 +26,3 @@ class Autoloader
 
     }
 }
-//class Autoload {
-//	public function __construct() {
-//
-//		spl_autoload_register(function ($class_name){
-//			$models_path = './Models/' . $class_name . '.php';
-//			$controllers_path = './Controllers/' . $class_name . '.php';
-//			$views_path = './Views/' . $class_name . '.php';
-//
-//			if( file_exists($models_path) )  require_once($models_path);
-//			if( file_exists($controllers_path) )  require_once($controllers_path);
-//			if( file_exists($views_path) )  require_once($views_path);
-//		});
-//	}
-//
-//	// public function __destruct() {
-//	// 	unset($this);
-//	// }
-//}
-
-
-// function autoload($classname){
-  
-//     if (file_exists($file = __DIR__ . '/' . $classname . '.php')){
-        
-//         require $file;
-//     }
-// }
-
-// spl_autoload_register('autoload');
